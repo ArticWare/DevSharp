2df204ed98994b8aadadeee454c09113ce19d536
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2024-07-05 11:45:25 +0700

    bre

---
+++ getstarted/ifsignedin.js
@@ -0,0 +1,29 @@
+const signedin = '../home';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6lx-kvf6kkTbSTuFywBGVCs8a0B_ZV-A",
+  authDomain: "devsharp-15542.firebaseapp.com",
+  projectId: "devsharp-15542",
+  storageBucket: "devsharp-15542.appspot.com",
+  messagingSenderId: "329676502356",
+  appId: "1:329676502356:web:5a97ed37c16b74d462c4c1",
+  measurementId: "G-D8H3TC9PMP"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+onAuthStateChanged(auth,function(user) {
+if (user) {
+    var email = user.email;
+    window.location.replace(signedin)
+}
+});
---
+++ home/ifsignedin.js
@@ -0,0 +1,28 @@
+const notsignedin = '../getstarted';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6lx-kvf6kkTbSTuFywBGVCs8a0B_ZV-A",
+  authDomain: "devsharp-15542.firebaseapp.com",
+  projectId: "devsharp-15542",
+  storageBucket: "devsharp-15542.appspot.com",
+  messagingSenderId: "329676502356",
+  appId: "1:329676502356:web:5a97ed37c16b74d462c4c1",
+  measurementId: "G-D8H3TC9PMP"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+onAuthStateChanged(auth,function(user) {
+if (!user) {
+    window.location.replace(notsignedin)
+}
+});
