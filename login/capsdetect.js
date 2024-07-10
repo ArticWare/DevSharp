86a3b3073d263d36417271e3e7158f53b4aba1ee
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2024-07-10 12:08:58 +0700

    Adsense

--- login/capsdetect.js
+++ login/capsdetect.js
@@ -1,7 +1,7 @@
 document.onkeydown = function (event) {
     event = event || window.event;
     const capswarning = document.getElementById('warning')
-    if(event.getModifierState('CapsLock')) {
+    if (event.getModifierState('CapsLock')) {
      capswarning.hidden = false;
     } else {
       capswarning.hidden = true;
